6a25b7bcc2289e513f54642b464f2d52729c37ef
Maximilian Schlenz <mschlenz@student.42heilbronn.de>
2023-04-07 10:26:20 +0200

    improved mysql setup

--- srcs/requirements/mariadb/test.sh
+++ srcs/requirements/mariadb/test.sh
@@ -2,14 +2,22 @@
 
 exec mysqld --user=mysql --skip-networking=0 --bind-address=0.0.0.0 &
 
-sleep 3
+sleep 6
 
-mariadb -u root << EOF
-CREATE DATABASE wp_db;
-CREATE USER 'dbuser'@'localhost' IDENTIFIED BY 'dbpw';
-GRANT ALL ON wp_db.* TO 'dbuser'@'%' IDENTIFIED BY 'dbpw';
+env
+
+
+mariadb -u root -p$MYSQL_ROOT_PASSWORD << EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+CREATE USER '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_ROOT_USER'@'%';
+CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
 EOF
 
 mysqladmin shutdown
+sleep 6
 
 exec mysqld --user=mysql --skip-networking=0 --bind-address=0.0.0.0
--- srcs/requirements/wordpress/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wordpress/wp-config.php
@@ -41,7 +41,7 @@ if (!function_exists('getenv_docker')) {
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv_docker('DB_NAME', 'wp_db') );
+define( 'DB_NAME', getenv_docker('DB_NAME', 'wordpress') );
 
 /** Database username */
 define( 'DB_USER', getenv_docker('DB_USER', 'dbuser') );
